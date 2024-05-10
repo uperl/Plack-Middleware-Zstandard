dd7291e3d2305e3d843131c636858eac34af8d78
Graham Ollis <plicease@cpan.org>
2024-05-09 19:56:36 -0600

    inital implemantation

--- lib/Plack/Middleware/Zstandard.pm
+++ lib/Plack/Middleware/Zstandard.pm
@@ -5,7 +5,52 @@ use experimental qw( postderef signatures );
 package Plack::Middleware::Zstandard {
 
   # ABSTRACT: Compress response body with Zstandard
+
+  use parent qw( Plack::Middleware );
+  use Plack::Util ();
+  use Ref::Util qw( is_arrayref );
+  use Compress::Stream::Zstd::Compressor ();
+
+  sub prepare_app ($self) {
+  }
+
+  sub call ($self, $env) {
+
+    my $res = $self->app->($env);
+
+    $self->response_cb($res, sub ($res) {
+      return undef if $env->{HTTP_CONTENT_RANGE};
+
+      my $h = Plack::Util::headers($res->[1]);
+      return undef if Plack::Util::status_with_no_entity_body($res->[0]);
+      return undef if $h->exists('Cache-Control') && $h->get('Cache-Control') =~ /\bno-transform\b/;
+
+      my @vary = split /\s*,\s*/, ($h->get('Vary') || '');
+      push @vary, 'Accept-Encoding';
+      $h->set('Vary' => join(",", @vary));
+
+      return undef unless ($env->{HTTP_ACCEPT_ENCODING} // '') =~ /\bzstd\b/;
+
+      $h->set('Content-Encoding' => 'zstd');
+      $h->remove('Content-Length');
+
+      my $compressor = Compress::Stream::Zstd::Compressor->new;
+
+      if($res->[2] && is_arrayref $res->[2]) {
+        my @buf = grep length, map { $compressor->compress($_) } grep defined, $res->[2]->@*;
+        my $end = $compressor->end;
+        push @buf, $end if length $end;
+        $res->[2] = \@buf;
+        return undef;
+      } else {
+        return sub ($content) {
+          # TODO: what about end?
+          return $compressor->compress($content);
+        };
+      }
+    });
+  }
+
 }
 
 1;
-
--- t/plack_middleware_zstandard.t
+++ t/plack_middleware_zstandard.t
@@ -1,8 +1,44 @@
 use Test2::V0 -no_srand => 1;
-use Plack::Middleware::Zstandard;
+use Plack::Builder;
+use Test2::Tools::HTTP qw( :short psgi_app_guard );
+use HTTP::Request::Common;
+use Compress::Stream::Zstd::Decompressor;
 
-ok 1, 'todo';
+subtest 'basic' => sub {
+
+  my $content = 'Hello World';
+
+  my $app = psgi_app_guard builder {
+    enable 'Zstandard';
+    sub { [200, [ 'Content-Type' => 'text/plain', 'Content-Length' => length($content) ], [ $content ]] };
+  };
+
+  req(
+    GET('/', 'Accept-Encoding' => 'zstd'),
+    res {
+      code 200;
+      content_type 'text/plain';
+      header 'Content-Length' => DNE();
+      header 'Content-Encoding' => 'zstd';
+      header 'Vary', 'Accept-Encoding';
+    },
+  );
+
+  is(
+    decompress(),
+    'Hello World',
+    'content',
+  );
+
+};
+
+sub decompress {
+  note $_ for map { $_->as_string} (tx->req, tx->res->headers);
+  note '';
+  my $decompressor = Compress::Stream::Zstd::Decompressor->new;
+  my $decoded_content = $decompressor->decompress(tx->res->content);
+  note $decoded_content;
+  return $decoded_content;
+}
 
 done_testing;
-
-
